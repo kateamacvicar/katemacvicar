819c85750d83f1de5f04fb2fc3b0f2e03f872b06
kate macvicar <kateamacvicar@gmail.com>
2022-05-30 14:33:47 -0500

    trying to add JS

--- slider.js
+++ slider.js
@@ -1,55 +1,28 @@
-$(function() {
+var slidePosition = 1;
+SlideShow(slidePosition);
 
-    new Slider({
-        images: '.slider-1 img',
-        btnPrev: '.slider-1 .buttons .prev',
-        btnNext: '.slider-1 .buttons .next',
-        auto: false
-    });
-});
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
 
+//  images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
 
-function Slider(obj) {
-
-  this.images = $(obj.images);
-  this.auto = obj.auto;
-  this.btnPrev = obj.btnPrev;
-  this.btnNext = obj.btnNext;
-    this.rate = obj.rate || 1000;
-
-  var i = 0;
-    var slider = this;
-
-    // The "Previous" button: to remove the class .shoved, show the previous image and add the .shoved class
-  this.prev = function () {
-    slider.images.eq(i).removeClass('shown');
-    i--;
-
-    if (i < 0) {
-      i = slider.images.length - 1;
-    }
-
-    slider.images.eq(i).addClass('shown');
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-
-    // The "Next" button: to remove the class .shoved, show the next image and add the .shoved class
-  this.next = function () {
-    slider.images.eq(i).removeClass('shown');
-    i++;
-
-    if (i >= slider.images.length) {
-      i = 0;
-    }
-
-    slider.images.eq(i).addClass('shown');
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
   }
-
-    // To add next and prev functions when clicking the corresponding buttons
-    $(slider.btnPrev).on('click', function(){ slider.prev();});
-    $(slider.btnNext).on('click', function(){ slider.next();});
-
-    // For the automatic slider: this method calls the next function at the set rate
-/*   if (slider.auto)  {
-        setInterval(slider.next, slider.rate);
-    } */
-};
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
+} 
